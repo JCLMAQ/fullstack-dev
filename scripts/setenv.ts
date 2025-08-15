7599cb66875b70460b5ba3b415e0a58138c2c948
JCM <jcl.maquinay@gmail.com>
2025-08-15 17:22:03 +0200

    add config proxy

---
+++ scripts/setenv.ts
@@ -0,0 +1,44 @@
+// const { writeFile } = require('fs');
+import { writeFile } from 'fs';
+// const { argv } = require('yargs');
+// read environment variables from .env file
+// require('dotenv').config();
+import dotenv from 'dotenv';
+const config = dotenv.config()
+// read the command line arguments passed with yargs
+// const argv = yargs.environemnt;
+
+const environment = process.env.NODE_ENV || 'dev';
+const isProduction = environment === 'prod';
+
+if ( !process.env.API_FRONTEND || !process.env.API_FRONTEND_PORT || !process.env.API_BACKEND || !process.env.API_BACKEND_PORT || !process.env.API_SECRET || !process.env.AUTO_REGISTRATION_ENABLE || !process.env.REGISTRATION_VALIDATION || !process.env.PWDLESS_LOGIN_ENABLE || !process.env.DEFAULT_LANGUAGE || !process.env.SUPPORTED_LANGUAGE) {
+  console.error('All the required environment variables were not provided!');
+  process.exit(-1);
+}
+
+const targetPath = isProduction
+    ? `./apps/frontend/jcm-app/environments/environment.prod.ts`
+    : `./apps/frontend/jcm-app/environments/environment.ts`;
+// we have access to our environment variables
+// in the process.env object thanks to dotenv
+const environmentFileContent = `
+    export const environment = {
+        production: ${isProduction},
+        API_URL_BACKEND: "http://${process.env.API_BACKEND}:${process.env.API_BACKEND_PORT}",
+        API_URL_FRONTEND: "http://${process.env.API_FRONTEND}:${process.env.API_FRONTEND_PORT}",
+        API_FRONTEND: "${process.env.API_FRONTEND}",
+        API_PORT: "${process.env.API_PORT}",
+        API_SECRET: "${process.env.API_SECRET}",
+        AUTO_REGISTRATION_ENABLE: "${process.env.AUTO_REGISTRATION_ENABLE}",
+        REGISTRATION_VALIDATION: "${process.env.REGISTRATION_VALIDATION}",
+        PWDLESS_LOGIN_ENABLE: "${process.env.PWDLESS_LOGIN_ENABLE}",
+        defaultLanguage: "${process.env.DEFAULT_LANGUAGE}",
+        supportedLanguages: ${process.env.SUPPORTED_LANGUAGE}
+    };`;
+// write the content to the respective file
+writeFile(targetPath, environmentFileContent, function (err: any) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(`Wrote variables to ${targetPath}`);
+});
