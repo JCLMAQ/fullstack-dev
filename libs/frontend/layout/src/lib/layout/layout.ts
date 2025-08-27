fd50f2acf465de13d493af30442ee4613443067a
JCM <jcl.maquinay@gmail.com>
2025-08-27 16:25:43 +0200

    add material

--- libs/frontend/layout/src/lib/layout/layout.ts
+++ libs/frontend/layout/src/lib/layout/layout.ts
@@ -1,10 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 
 @Component({
   selector: 'lib-layout',
-  imports: [],
+  imports: [
+    MatSidenavModule,
+    MatListModule,
+    RouterModule,
+    MatIconModule,
+    MenuItem,
+    SidenavHeader
+  ],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Layout {}
+export class Layout {
+  responsiveService = inject(ResponsiveService);
+
+  collapsed = computed(() => this.responsiveService.isCollapsed());
+
+  menuItems = menuItems;
+}
