3b4e1339782ece0591384e3d6b2ccb55e5f231c9
JCM <jcl.maquinay@gmail.com>
2025-09-12 15:40:16 +0200

    Add user and post nestjs and prisma utilities

---
+++ libs/backend/data-access/posts/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/posts.service';
+export * from './lib/posts.module';
---
+++ libs/backend/data-access/posts/src/lib/posts.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { PostsService } from './posts.service';
+
+@Module({
+  controllers: [],
+  providers: [PostsService],
+  exports: [PostsService],
+})
+export class PostsModule {}
