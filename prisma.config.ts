5fb5d0a06c1a55c72fdb29b2f008927837f2a1c0
JCM <jcl.maquinay@gmail.com>
2025-09-12 14:20:12 +0200

    Add prisma db

---
+++ prisma.config.ts
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import path from "node:path";
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: path.join("libs/db/prisma", "schema.prisma"),
+  migrations: {
+    path: path.join("libs/db", "migrations"),
+  },
+  views: {
+    path: path.join("libs/db/views", "views"),
+  },
+  typedSql: {
+    path: path.join("libs/db/queries", "queries"),
+  }
+});
