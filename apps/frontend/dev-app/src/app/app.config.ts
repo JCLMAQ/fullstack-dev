1ff85fa475e5d08b88524f65378073a7f9021a16
JCM <jcl.maquinay@gmail.com>
2025-09-25 17:40:28 +0200

    Start implement lyaout

--- apps/frontend/dev-app/src/app/app.config.ts
+++ apps/frontend/dev-app/src/app/app.config.ts
@@ -3,13 +3,33 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 import { appRoutes } from './app.routes';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZonelessChangeDetection(),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(appRoutes),
+    provideRouter(appRoutes, withComponentInputBinding()),
+
+
+    provideTranslateService({
+      fallbackLang: 'en',
+      loader: provideTranslateHttpLoader({
+        prefix:"i18n/",
+        suffix:".json",
+        enforceLoading: true,
+        useHttpBackend: true,
+      }),
+    }),
+
+    provideNativeDateAdapter(),
+
+
   ],
 };
