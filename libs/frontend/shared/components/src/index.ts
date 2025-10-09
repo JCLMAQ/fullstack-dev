3229ca625e59e0b61c117a0637a3a0a7ba4b0729
JCM <jcl.maquinay@gmail.com>
2025-10-09 13:26:33 +0200

    Try to reorganise lib

--- libs/frontend/shared/components/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './lib/dashboard/dashboard';
-// export * from './lib/image-uploader/image-uploader';
-export * from './lib/lib.routes';
-// export * from './lib/loading-container/loading-container';
-// Messages moved to @fe/services
-
--- libs/frontend/shared/components/src/lib/messages/message.model.ts
+++
@@ -1,8 +0,0 @@
-export type MessageSeverity =
-  "error" | "warning" | "info" | "success";
-
-export type Message = {
-  severity: MessageSeverity;
-  text: string;
-}
-
