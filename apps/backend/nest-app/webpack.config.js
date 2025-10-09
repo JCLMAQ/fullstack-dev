3bb550e68d553a443bcd1e809862171d39d71ab1
JCM <jcl.maquinay@gmail.com>
2025-10-09 12:16:06 +0200

    Backed rerunning

--- apps/backend/nest-app/webpack.config.js
+++ apps/backend/nest-app/webpack.config.js
@@ -1,7 +1,22 @@
 const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
-const { join } = require('path');
+const { join, resolve } = require('path');
 
 module.exports = {
+  target: 'node',
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+      // Backend libs
+      '@be/posts': resolve(__dirname, '../../../libs/backend/data-access/posts/src/index.ts'),
+      '@be/users': resolve(__dirname, '../../../libs/backend/data-access/users/src/index.ts'),
+      // DB libs
+      '@db/prisma': resolve(__dirname, '../../../libs/db/index.ts'),
+      '@db/prisma-client': resolve(
+        __dirname,
+        '../../../libs/backend/prisma-client/src/index.ts'
+      ),
+    },
+  },
   output: {
     path: join(__dirname, '../../../dist/apps/backend/nest-app'),
     ...(process.env.NODE_ENV !== 'production' && {
