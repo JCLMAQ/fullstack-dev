009e00b5ed1465f3cdd8395c0b52c5bc14b7e15d
JCM <jcl.maquinay@gmail.com>
2025-10-14 11:42:08 +0200

    backend controloer suite (2)

--- libs/backend/data-access/groups/src/lib/groups.module.ts
+++ libs/backend/data-access/groups/src/lib/groups.module.ts
@@ -1,8 +1,10 @@
+import { PrismaClientModule } from '@db/prisma-client';
 import { Module } from '@nestjs/common';
 import { GroupsController } from './groups.controller';
 import { GroupsService } from './groups.service';
 
 @Module({
+  imports: [PrismaClientModule],
   controllers: [GroupsController],
   providers: [GroupsService],
   exports: [GroupsService],
--- libs/backend/data-access/postLikes/src/lib/postLikes.module.ts
+++ libs/backend/data-access/postLikes/src/lib/postLikes.module.ts
@@ -1,8 +1,10 @@
+import { PrismaClientModule } from '@db/prisma-client';
 import { Module } from '@nestjs/common';
 import { PostLikesController } from './postLikes.controller';
 import { PostLikesService } from './postLikes.service';
 
 @Module({
+  imports: [PrismaClientModule],
   controllers: [PostLikesController],
   providers: [PostLikesService],
   exports: [PostLikesService],
