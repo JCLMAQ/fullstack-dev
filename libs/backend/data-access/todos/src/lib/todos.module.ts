e6deb32ebf9a1a8d5837872054f3926c36a52f49
JCM <jcl.maquinay@gmail.com>
2025-11-07 09:03:49 +0100

    ajout des fonctionnalités CRUD pour les utilisateurs et les todos, intégration de Prisma pour la gestion des données

--- libs/backend/data-access/todos/src/lib/todos.module.ts
+++ libs/backend/data-access/todos/src/lib/todos.module.ts
@@ -1,8 +1,10 @@
+import { PrismaClientModule } from '@db/prisma-client';
 import { Module } from '@nestjs/common';
 import { TodosController } from './todos.controller';
 import { TodosService } from './todos.service';
 
 @Module({
+  imports: [PrismaClientModule],
   controllers: [TodosController],
   providers: [TodosService],
   exports: [TodosService],
--- libs/backend/data-access/users/src/lib/users.module.ts
+++ libs/backend/data-access/users/src/lib/users.module.ts
@@ -1,8 +1,10 @@
+import { PrismaClientModule } from '@db/prisma-client';
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [PrismaClientModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
