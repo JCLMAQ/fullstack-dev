f65818bd3176b304515dbed5520dfe1896b71ec3
JCM <jcl.maquinay@gmail.com>
2025-11-10 10:27:44 +0100

    feat: refactor Address module with comprehensive CRUD operations and error handling

--- libs/backend/data-access/address/src/lib/address.module.ts
+++ libs/backend/data-access/address/src/lib/address.module.ts
@@ -1,8 +1,10 @@
+import { PrismaClientModule } from '@db/prisma-client';
 import { Module } from '@nestjs/common';
 import { AddressController } from './address.controller';
 import { AddressService } from './address.service';
 
 @Module({
+  imports: [PrismaClientModule],
   controllers: [AddressController],
   providers: [AddressService],
   exports: [AddressService],
