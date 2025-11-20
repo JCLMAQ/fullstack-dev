a492770b8c27bfc88a55098cea464965717276ad
JCM <jcl.maquinay@gmail.com>
2025-11-20 11:10:59 +0100

    Remove '@prisma/client' from onlyBuiltDependencies in pnpm-workspace.yaml

--- libs/backend/prisma-client/src/lib/prisma-client.service.ts
+++ libs/backend/prisma-client/src/lib/prisma-client.service.ts
@@ -1,9 +1,30 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@db/prisma';
-import { Injectable, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
-export class PrismaClientService extends PrismaClient implements OnModuleInit {
+export class PrismaClientService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+      const config: any = {
+      log: [
+        { level: 'query', emit: 'event' },
+        { level: 'error', emit: 'stdout' },
+        { level: 'info', emit: 'stdout' },
+        { level: 'warn', emit: 'stdout' },
+      ],
+    };
+
+    if (process.env.PRISMA_ACCELERATE_URL) {
+      config.accelerateUrl = process.env.PRISMA_ACCELERATE_URL;
+    }
+
+    super(config);
+  }
+
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
