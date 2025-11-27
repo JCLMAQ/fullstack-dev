e3bf2b65044de0f794e5644d0e110575c2944a91
JCM <jcl.maquinay@gmail.com>
2025-11-27 16:45:23 +0100

    feat(admin-banner): ajouter un message de connexion pour les administrateurs

--- libs/frontend/shared-misc/components/admin-banner/src/lib/admin-banner/admin-banner.ts
+++ libs/frontend/shared-misc/components/admin-banner/src/lib/admin-banner/admin-banner.ts
@@ -1,9 +1,10 @@
 import { Component, computed, inject } from '@angular/core';
 import { AppStore } from '@fe/stores';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'lib-admin-banner',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './admin-banner.html',
   styleUrl: './admin-banner.scss',
 })
