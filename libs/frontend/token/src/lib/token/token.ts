a258bc13166e00290e7b2509e980243d0afaac9d
JCM <jcl.maquinay@gmail.com>
2025-12-01 11:23:43 +0100

    feat(token): ajouter la bibliothèque de token avec configuration, tests et composants

---
+++ libs/frontend/token/src/lib/token/token.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-token',
+  imports: [],
+  templateUrl: './token.html',
+  styleUrl: './token.css',
+})
+export class Token {}
