0d43b3b9f8dcec12a2da216e94b3e4f224c13e16
JCM <jcl.maquinay@gmail.com>
2025-12-05 15:53:25 +0100

    refactor: mettre à jour les importations pour utiliser les nouveaux tokens et supprimer les anciens

--- libs/frontend/core/shared/src/lib/tokens/index.ts
+++ libs/frontend/core/shared/src/lib/tokens/index.ts
@@ -4,4 +4,6 @@ export * from './models/menu-items.model';
 // export * from './tokens/dictionaries.token';
 // export * from './tokens/environement.token';
 // export * from './tokens/iam-auth.token';
+export * from './tokens/dictionaries.token';
 export * from './tokens/menu.token';
+
