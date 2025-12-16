906d1e7be65af9dfca31a65390fc75b61f1cea46
JCM <jcl.maquinay@gmail.com>
2025-12-16 17:16:03 +0100

    feat(changepwd): refactor sendEmailForgotPwd to use HttpClient for API call

--- libs/frontend/core/auth/src/lib/iam-auth/services/changepwd/changepwd-service.ts
+++ libs/frontend/core/auth/src/lib/iam-auth/services/changepwd/changepwd-service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ENVIRONMENT_TOKEN } from '@fe/tokens';
+import { firstValueFrom } from 'rxjs';
+import { ILoginResponse } from '../../../models/auth.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChangePwdService {
 
-  constructor() { }
+  private readonly httpClient = inject(HttpClient);
+  private readonly environment = inject(ENVIRONMENT_TOKEN);
+
+  async sendEmailForgotPwd(email: string): Promise<{ message: string }> {
+    const apiPrefix = this.environment.API_BACKEND_PREFIX?.replace(/^\//, '').replace(/\/$/, '');
+        const pathUrl = `${apiPrefix}/authentication/forgot-password`;
+
+        console.log('🔐 Attempting forgot password email sending:', email);
+
+        const sendEmailForgotPwd$ = this.httpClient.post<ILoginResponse>(`${pathUrl}`, {
+          email,
+          password,
+        });
+
+        const loginResponse = await firstValueFrom(sendEmailForgotPwd$);
+              console.log('✅ sendEmailForgotPwd sended:', {
+
+              });
+
+    // Simulate an API call to send a password reset email
+    // return new Promise((resolve) => {
+    //   setTimeout(() => {
+    //     resolve({ message: `Password reset email sent to ${email}` });
+    //   }, 1000);
+    // });
+
+    /*
+    export interface AuthResponse {
+      success: boolean;
+      message: string;
+}
+    */
+  }
 }
