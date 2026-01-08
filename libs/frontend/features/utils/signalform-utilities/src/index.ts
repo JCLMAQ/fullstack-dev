3e08643ce5f67f5492ed6c8f237f198ea092c546
JCM <jcl.maquinay@gmail.com>
2026-01-08 20:32:30 +0100

    feat: ajout de l'export du module projected-signal dans les utilitaires de formulaire

--- libs/frontend/features/utils/signalform-utilities/src/index.ts
+++ libs/frontend/features/utils/signalform-utilities/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/field-error/field-error';
 export * from './lib/password-match/password-match';
 export * from './lib/password-strength/password-strength';
+export * from './lib/projected-signal/projected-signal';
 export * from './lib/validators/credentials-schemas';
 
