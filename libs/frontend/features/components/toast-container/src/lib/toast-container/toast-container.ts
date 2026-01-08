0ea6245b18ad422684b53c80bbea49198eb4f2a2
JCM <jcl.maquinay@gmail.com>
2026-01-08 21:30:24 +0100

    feat: ajout du composant toast-container avec configuration et tests

---
+++ libs/frontend/features/components/toast-container/src/lib/toast-container/toast-container.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-toast-container',
+  imports: [],
+  templateUrl: './toast-container.html',
+  styleUrl: './toast-container.scss',
+})
+export class ToastContainer {}
