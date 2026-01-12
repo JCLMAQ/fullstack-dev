fcae0f2d246e0e6ceeb7e260cd5f4693063846c7
JCM <jcl.maquinay@gmail.com>
2026-01-12 18:27:57 +0100

    feat: mise à jour du composant AddressForm avec traduction et amélioration de l'interface utilisateur

--- libs/frontend/features/components/address/address-form/src/lib/address-form./address-form.ts
+++ libs/frontend/features/components/address/address-form/src/lib/address-form./address-form.ts
@@ -1,14 +1,20 @@
 import { Component, input } from '@angular/core';
 import { Field, FieldTree } from '@angular/forms/signals';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { Address } from '@db/prisma';
 import { ValidationErrors } from '@fe/signalform-utilities';
+import { TranslateModule } from '@ngx-translate/core';
 // import { buildAddressSection, createAddressModel } from './address-form.model';
 
 @Component({
   selector: 'lib-address-form',
   imports: [
     ValidationErrors,
-    Field
+    Field,
+    MatFormFieldModule,
+    MatInputModule,
+    TranslateModule,
   ],
   templateUrl: './address-form.html',
   styleUrl: './address-form.scss',
