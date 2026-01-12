dd5340581b98fb1b179d696874e0933b9ba939d7
JCM <jcl.maquinay@gmail.com>
2026-01-12 11:50:10 +0100

    feat: mise à jour du composant FieldError pour utiliser un type inconnu et ajout de la traduction des messages d'erreur

--- libs/frontend/features/utils/signalform-utilities/src/lib/field-error/field-error.ts
+++ libs/frontend/features/utils/signalform-utilities/src/lib/field-error/field-error.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { FieldState } from '@angular/forms/signals';
 import { TranslateModule } from '@ngx-translate/core';
 
+
 @Component({
   selector: 'lib-field-error',
   imports: [TranslateModule],
@@ -10,5 +11,5 @@ import { TranslateModule } from '@ngx-translate/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FieldError {
-  fieldError = input.required<FieldState<any, any>>();
+  fieldError = input.required<FieldState<unknown, string>>();
 }
