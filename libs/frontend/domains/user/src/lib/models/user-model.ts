2e9554d13bdafe63d97ac601fad32b58fdca6667
JCM <jcl.maquinay@gmail.com>
2026-01-13 21:44:17 +0100

    fix: mise à jour de l'affichage de l'ID utilisateur dans le détail de l'utilisateur pour utiliser la valeur du formulaire

--- libs/frontend/domains/user/src/lib/models/user-model.ts
+++
@@ -1,8 +0,0 @@
-import { Address, Organization, User } from "@db/prisma";
-
-export type UserWithRelations = User & {
-  Address: Address[];
-  Orgs: Organization[];
-  Followers: User[];
-  Followings: User[];
-};
