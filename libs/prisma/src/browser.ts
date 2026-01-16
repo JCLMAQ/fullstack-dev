a040ab84e238b75e332a011975f1c6e57fdb615b
JCM <jcl.maquinay@gmail.com>
2026-01-16 16:54:09 +0100

    feat: ajouter l'export du PrismaClient dans le fichier client.ts

--- libs/prisma/src/browser.ts
+++ libs/prisma/src/browser.ts
@@ -7,3 +7,4 @@ export * from './lib/generated/prisma/commonInputTypes';
 export * from './lib/generated/prisma/enums';
 export * from './lib/generated/prisma/models';
 export * from './lib/userWithRelations-model';
+
--- libs/prisma/src/client.ts
+++ libs/prisma/src/client.ts
@@ -2,5 +2,6 @@
  * Backend-only Prisma Client export
  * Use this only in backend code
  */
-export { PrismaClient } from './lib/generated/prisma/client';
 export * from './lib/generated/prisma/client';
+export { PrismaClient } from './lib/generated/prisma/client';
+
