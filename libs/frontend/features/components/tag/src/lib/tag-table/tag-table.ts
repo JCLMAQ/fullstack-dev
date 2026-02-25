15d3ef563dc9a6a4b0180d0e78b3745465683b67
JCM <jcl.maquinay@gmail.com>
2026-02-25 16:43:10 +0100

    feat: ajouter le service TagService et le composant TagTable avec des tests associés

---
+++ libs/frontend/features/components/tag/src/lib/tag-table/tag-table.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-tag-table',
+  imports: [],
+  templateUrl: './tag-table.html',
+  styleUrl: './tag-table.scss',
+})
+export class TagTable {}
--- libs/frontend/features/components/tag/src/lib/tag/tag.ts
+++ libs/frontend/features/components/tag/src/lib/tag/tag.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TagTable } from '../tag-table/tag-table';
 
 @Component({
   selector: 'lib-tag',
-  imports: [],
+  imports: [TagTable],
   templateUrl: './tag.html',
   styleUrl: './tag.scss',
 })
